c95b71130b2fcfc73dce61b59774d245e2fb62c6
iafzalhameed <46992650+iafzalhameed@users.noreply.github.com>
2021-04-30 04:09:07 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -64,3 +64,21 @@ ityped.init(document.querySelector("#ityped"), {
 
 /*DISABLE RIGHT CLICK */
 /* document.addEventListener('contextmenu', event => event.preventDefault());*/
+const options = {
+
+
+  bottom: '32px', // default: '32px'
+  right: '32px', // default: '32px'
+  left: 'unset', // default: 'unset'
+  time: '0.3s', // default: '0.3s'
+  mixColor: '#fff', // default: '#fff'
+  backgroundColor: '#fff',  // default: '#fff'
+  buttonColorDark: '#100f2c',  // default: '#100f2c'
+  buttonColorLight: '#fff', // default: '#fff'
+  saveInCookies: true, // default: true,
+  label: '🌓', // default: ''
+  autoMatchOsTheme: true, // default: true
+}
+
+const darkmode = new Darkmode(options);
+darkmode.showWidget();
