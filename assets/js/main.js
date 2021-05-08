d4f0d806951de9824d9be574fbf582b7c9abb896
Afzal Hameed <46992650+dev-afzal@users.noreply.github.com>
2021-05-08 19:13:01 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -226,21 +226,3 @@ $('.isotope-pager').on( 'click', 'a', function() {
 });
     })
 	
-
-$('.brand-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    autoplay:true,
-    responsive:{
-      0:{
-        items:1
-      },
-      600:{
-        items:3
-      },
-      1000:{
-        items:5
-      }
-    }
-  }) 
-  
